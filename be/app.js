42bd132ca79a606dc88a62bbc0fd09f213a36eb8
thanh-1504 <114299189+thanh-1504@users.noreply.github.com>
2025-02-17 20:09:24 +0700

    Change origin cors

--- be/app.js
+++ be/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "1mb" }));
 app.use(
   cors({
-    origin: "https://lucent-sprinkles-cfc10b.netlify.app/",
+    origin: "https://cliconapp.netlify.app",
     credentials: true,
   })
 );
