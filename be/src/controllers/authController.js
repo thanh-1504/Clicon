1f1a79299bd9f21daeb8326e0da5a7111a0b2bbf
thanh-1504 <114299189+thanh-1504@users.noreply.github.com>
2025-02-18 08:42:54 +0700

    Update authController.js

--- be/src/controllers/authController.js
+++ be/src/controllers/authController.js
@@ -29,8 +29,7 @@ const logInAndSendUserWithToken = (res, user, statusCode) => {
   const token = signToken(user._id);
   const cookieOption = {
     // secure: process.env.NODE_ENV === "production",
-    secure:true,
-    sameSite: "none",
+    secure: true,
     domain: ".onrender.com",
     expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
   };
