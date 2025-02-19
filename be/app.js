f5fbff9bb53662aba174a9441d9305c39d8e27e0
thanh-1504 <114299189+thanh-1504@users.noreply.github.com>
2025-02-19 15:47:02 +0700

    Update app.js

--- be/app.js
+++ be/app.js
@@ -1,6 +1,11 @@
-const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const express = require("express");
 const cookieParser = require("cookie-parser");
+const sanitizeHtml = require("sanitize-html");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 const cartRouter = require("./src/routes/cartRouter");
 const userRouter = require("./src/routes/userRouter");
 const reviewRouter = require("./src/routes/reviewRouter");
@@ -8,12 +13,34 @@ const productRouter = require("./src/routes/productRouter");
 const paymentStripeRouter = require("./src/routes/paymentStripeRouter");
 const { globalError } = require("./src/controllers/globalErrorController");
 const app = express();
+app.use(helmet());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this ip, please try again in an hour!",
+});
 app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(express.json({ limit: "1mb" }));
+app.use(mongoSanitize());
+app.use((req, res, next) => {
+  if (req.body) {
+    for (const key in req.body) {
+      if (typeof req.body[key] === "string") {
+        req.body[key] = sanitizeHtml(req.body[key], {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+      }
+    }
+  }
+  next();
+});
+app.use(hpp());
+app.use("/api", limiter);
 app.use(
   cors({
-    origin: "https://clicon-abfr.onrender.com",
+    origin: "https://cliconshop.onrender.com",
     credentials: true,
   })
 );
