82adfb5c9d4c331dc2c5f68502fa6bd6201e1896
thanh-1504 <114299189+thanh-1504@users.noreply.github.com>
2025-03-08 11:28:31 +0700

    Update paymentStripeController.js

--- be/src/controllers/paymentStripeController.js
+++ be/src/controllers/paymentStripeController.js
@@ -39,8 +39,8 @@ exports.paymentWithStripe = catchAsync(async (req, res) => {
     mode: "payment",
     //success_url: `${req.protocol}://clicon-abfr.onrender.com/checkout-success`,
     // cancel_url: `${req.protocol}://clicon-abfr.onrender.com/cart`,
-     success_url: `${req.protocol}://${req.get('origin')}/checkout-success`,
-  cancel_url: `${req.protocol}://${req.get('origin')}/cart`,
+    success_url: `${req.get('origin')}:/checkout-success`,
+    cancel_url: `${req.get('origin')}:/cart`,
   });
 
   res.status(200).json({
